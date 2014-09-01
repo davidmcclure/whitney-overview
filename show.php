3623314c1bdca6b45d7b7c364e2de0f929d5f637
David McClure <davidwilliammcclure@gmail.com>
2014-09-01 11:00:19 -0700

    Basic styling on the narrative.

--- show.php
+++ show.php
@@ -9,23 +9,24 @@
 
 ?>
 
-<?php // queue_css_file('style'); ?>
-<?php // queue_js_file('show'); ?>
-
 <?php echo head(array(
   'title' => nl_getExhibitField('title'),
   'bodyclass' => 'neatline show'
 )); ?>
 
-<!-- Exhibit -->
+<!-- Exhibit. -->
 <div class="exhibit">
   <?php echo nl_getExhibitMarkup(); ?>
 </div>
 
 <div id="neatline-narrative" class="narrative">
 
-  <!-- Narrative -->
-  <h1 class="title"><?php echo nl_getExhibitField('title'); ?></h1>
+  <!-- Title. -->
+  <div class="page-header">
+    <h1><?php echo nl_getExhibitField('title'); ?></h1>
+  </div>
+
+  <!-- Narrative. -->
   <?php echo nl_getExhibitField('narrative'); ?>
 
 </div>
